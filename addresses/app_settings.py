6f320ea6d5b22e4cab5aa9fbe1ec8f0f8ce92e29
ollieanderson <ollieaa17@gmail.com>
2023-06-28 12:25:07 +0100

    Added abilty to import callables from app_settings file as well as path string

--- addresses/app_settings.py
+++ addresses/app_settings.py
@@ -5,13 +5,17 @@ from django.conf import settings
 from addresses.serializers import AddressSerializer, CountrySerializer, StateSerializer
 
 
-def import_from_path(path):
-    package, attr = path.rsplit('.', 1)
-    return getattr(import_module(package), attr)
+def import_callable_or_path(callable_or_path):
+    if hasattr(callable_or_path, '__call__'):
+        return callable_or_path
+    else:
+        assert isinstance(callable_or_path, str)
+        package, attr = callable_or_path.rsplit('.', 1)
+        return getattr(import_module(package), attr)
 
 
 settings = getattr(settings, 'ADDRESSES_SETTINGS', {})
 
-AddressSerializer = import_from_path(settings.get("ADDRESS_SERIALIZER", AddressSerializer))
-CountrySerializer = import_from_path(settings.get("COUNTRY_SERIALIZER", CountrySerializer))
-StateSerializer = import_from_path(settings.get("STATE_SERIALIZER", StateSerializer))
+AddressSerializer = import_callable_or_path(settings.get("ADDRESS_SERIALIZER", AddressSerializer))
+CountrySerializer = import_callable_or_path(settings.get("COUNTRY_SERIALIZER", CountrySerializer))
+StateSerializer = import_callable_or_path(settings.get("STATE_SERIALIZER", StateSerializer))
